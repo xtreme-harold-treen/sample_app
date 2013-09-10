cb251ff96d55fc3499886c867c75dd703040ae18
xtreme-harold-treen <harold.treen@xtremelabs.com>
2013-09-09 20:07:51 -0400

    End of Chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,18 @@ describe "User pages" do
 			end
 		end
 
+		describe "after submission" do
+			before { click_button submit }
+
+			it { should have_title('Sign up') }
+			it { should have_content('error') }
+			it { should have_content("Password can't be blank") }
+			it { should have_content("Password is too short") }
+			it { should have_content("Name can't be blank") }
+			it { should have_content("Email can't be blank") }
+			it { should have_content("Email is invalid") }
+		end
+
 		describe "with valid information" do
 			before do
 				fill_in "Name",			with: "Example User"
@@ -39,6 +51,14 @@ describe "User pages" do
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: "user@example.com") }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
